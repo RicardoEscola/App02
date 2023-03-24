add419849ad8baa788eb8b6b1ee978c04e6d550d
20211TIIMI0265 <ricardoleiteescola@gmail.com>
2023-03-24 10:25:27 -0300

    Codigo comentado

--- app/src/main/java/rodrigues/leite/app02/MainActivity.java
+++ app/src/main/java/rodrigues/leite/app02/MainActivity.java
@@ -26,17 +26,22 @@ public class MainActivity extends AppCompatActivity {
                 EditText etAssunto = findViewById(R.id.etAssunto);
                 EditText etTexto = findViewById(R.id.etTexto);
 
+                // Guardando os dados em variaveis
                 String email = etEmail.getText().toString();
                 String assunto = etAssunto.getText().toString();
                 String texto = etTexto.getText().toString();
 
+                // Criando uma intent, como não exite mais uma tela ele irá para o link indicado no setData que é o do email
                 Intent i = new Intent(Intent.ACTION_SENDTO);
                 i.setData(Uri.parse("mailto:"));
 
+                //Mandando os valores das variaveis para os campos do email, como: email, assunto e o texto
                 String[] emails = new String[]{email};
                 i.putExtra(Intent.EXTRA_EMAIL, emails);
                 i.putExtra(Intent.EXTRA_SUBJECT, assunto);
                 i.putExtra(Intent.EXTRA_TEXT, texto);
+
+                // Caso o usuário tenha mais apps que aceitam o compartilhamento, caso tenha você escolhe o app, caso não, o android te mostra a mensagem de erro
                 try {
                     startActivity(Intent.createChooser(i, "Escolha o APP"));
                 } catch (ActivityNotFoundException e) {
